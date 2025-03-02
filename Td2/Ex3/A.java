15a07c10932f50ec4555d8128016b12d023835fc
Young Wolf <162513105+kingofdead6@users.noreply.github.com>
2025-03-02 13:29:55 +0100

    Add files via upload

---
+++ Td2/Ex3/A.java
@@ -0,0 +1,7 @@
+package Ex3; // Define the package name
+
+public class A {
+    public void f(double x) {
+        System.out.println("A.f(double=" + x + ")");
+    }
+}
---
+++ Td2/Ex3/D.java
@@ -0,0 +1,7 @@
+package Ex3; // Define the package name
+
+public class D extends C {
+    public void f(int n) {
+        System.out.println("D.f(int=" + n + ")");
+    }
+}
