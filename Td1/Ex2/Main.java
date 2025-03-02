b1ecb5131cc9295fa9f470a37e8410880efb1948
Young Wolf <162513105+kingofdead6@users.noreply.github.com>
2025-03-02 13:23:13 +0100

    Create Main.java

---
+++ Td1/Ex2/Main.java
@@ -0,0 +1,10 @@
+package Ex2;
+
+public class Main {
+    public static void main(String[] args) {
+        Point p = new Point(3, 5);
+        p.display();
+        p.move(2, 3);
+        p.display();
+    }
+}
